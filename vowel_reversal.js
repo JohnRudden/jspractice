5ba140ec327f1e8c1070131d6fc79e93c78cfd8a
J.Rudden <52506030+JR-commits@users.noreply.github.com>
2025-01-14 10:48:31 +0000

    Initial commit - Vowel Reversal

---
+++ vowel_reversal.js
@@ -0,0 +1,34 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const isVowel = (input) =>
+  ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"].includes(input);
+
+rl.question(`Enter phrase for vowel reversal?\n -> `, (phrase) => {
+  console.log(`Your chosen phrase is! ${phrase}`);
+
+  const vowelArray = [];
+  let vowelIndex = [];
+  const phraseArray = [...phrase];
+
+  // Cycle through the phrase and capture the vowels and their respective indexes
+  phraseArray.forEach((item, index) => {
+    if (isVowel(item)) {
+      vowelArray.push(item);
+      vowelIndex.unshift(index); // reversing the index sequence
+    }
+  });
+
+  var ind = 0;
+  vowelArray.forEach((item) => {
+    phraseArray[vowelIndex[ind]] = item;
+    ind++;
+  });
+
+  console.log("Result of reversing the vowels is > ", phraseArray.join(""));
+  rl.close();
+});
